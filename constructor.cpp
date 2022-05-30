6e83bad6f8fb770ff98d7457cde9713723236e87
Deepanshi Dey <92084743+Deepanshi03@users.noreply.github.com>
2022-05-30 19:17:28 +0530

    Add files via upload

---
+++ constructor.cpp
@@ -0,0 +1,41 @@
+// #include<iostream>
+// using namespace std;
+// class complex{
+//     int a,b;
+//     public:
+    // complex(){ //Default Constructor
+    //     a=0;
+    //     b=0;
+    // }
+    // complex(int a1){ //Parameterized Constructor
+    //     a=a1;
+    //     b=0;
+    // }
+    // complex(int a1=5,int b1=7){ //Constructor with default argument
+    //     a=a1;
+    //     b=b1;
+    // }
+    // complex(int a1,int b1){ //Constructor Overloading
+    //     a=a1;
+    //     b=b1;
+    // }
+    // void putdata(){
+    //     cout<<a<<"+i"<<b;
+    // }
+    // ~complex(){
+    //     cout<<"this is a destructor"<<endl
+    // }; //Destructor
+//     complex(complex &obj){ //Copy constructor
+//         a=obj.a;
+//         b=obj.b;
+//     }
+// };
+// int main()
+// {
+//     complex c1;
+//     // complex c1(3,7);
+//     complex c2=c1;
+//     // complex c2(c1);
+//     c2.putdata();
+//     return 0;
+// }
